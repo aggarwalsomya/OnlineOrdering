137a323536fbd0666a65e31d4d004c877f2d95d8
aggarwalsomya <aggarwal.somya@gmail.com>
2016-05-02 14:55:35 -0700

    Adding the method to return all menu items for all categories.
    Adding the method to return all menu items for all categories.

--- src/main/java/com/cmpe275/OnlineOrdering/UserController.java
+++ src/main/java/com/cmpe275/OnlineOrdering/UserController.java
@@ -11,6 +11,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class UserController {
+
+	public final String MAINCOURSE = "maincourse";
+	public final String DESERT = "desert";
+	public final String APPETIZER = "appetizer";
+	public final String DRINK = "drink";
 	
 	@Autowired
 	private UserService userSvc;
@@ -32,6 +37,21 @@ public class UserController {
 		model.addAttribute("list", mi);
 		
 		return "GetUserMenuItems";
+	}
+	
+	public String getDataForEachCategory(Model model) {
+		String category[] = {MAINCOURSE,DRINK,DESERT,APPETIZER};
+		for(int i = 0; i < category.length; i++) {
+			List<MenuItem> mi = userSvc.getMenuItems(category[i]);		
+			model.addAttribute("list_"+category[i].toString(), mi);
+		}
+		return "GetUserMenuItems";
+	}
+	
+	
+	public String getEarliestPickUpTime() {
+		
+		return "";
 	}
 	
 	@RequestMapping(value = "/Menu", method = RequestMethod.GET)
